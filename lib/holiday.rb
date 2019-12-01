d472cb821406b87917157c667ccb4fbb18b2850d
wolfene2001 <wolfene2001@yahoo.com>
2019-12-01 19:02:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def all_winter_holiday_supplies(holiday_hash)
   #holiday_hash[:winter].map do |holiday_name, supply_array|
   holiday_hash[:winter].values.flatten
     end
-    
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
