21ec32a40c69fc8a2955cc600e18c2c8cf79c13d
wolfene2001 <wolfene2001@yahoo.com>
2019-12-01 19:09:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,5 +84,17 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  answer = []
+  holiday_hash.each do |season, data|
+    data.each do |holiday, value|
+      value.each do |supply|
+       if supply == "BBQ"
+          answer.push(holiday)
+        end
+      end
+    end
+  end
+  answer
+  
+  end
 
-end
